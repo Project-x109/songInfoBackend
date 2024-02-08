074b381e9780f86d4ad075ce3b277815691a3b44
Project-x109 <amanuelgirma109@gmail.com>
2024-02-08 20:57:59 +0300

    minors

--- server/controllers/songsController.js
+++ server/controllers/songsController.js
@@ -108,7 +108,9 @@ exports.getSongsCountByGenre = asyncErrorHandler(async (req, res) => {
     const error = [];
     try {
         const genreCounts = await Song.aggregate([
-            { $group: { _id: "$genre", count: { $sum: 1 } } }
+            { $group: { _id: "$genre", count: { $sum: 1 } } },
+            { $sort: { count: -1 } },
+            { $limit: 4}
         ]);
         res.status(200).json({ success: true, data: genreCounts });
     } catch (err) {
