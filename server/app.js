6fa007b0bccee25fb6ef9c4d220b109daa46f3df
Project-x109 <amanuelgirma109@gmail.com>
2024-02-08 21:38:09 +0300

    minor

--- server/app.js
+++ server/app.js
@@ -5,13 +5,13 @@ const app = express();
 const errorHandler = require("./middlewares/errorhandling.js");
 const { logMiddleware } = require("./middlewares/logMiddleware.js");
 const songs = require("./routes/songsRoute");
-const allowedOrigin = 'https://songlistbackend.onrender.com';
+/* const allowedOrigin = 'https://songlistbackend.onrender.com';
 const corsOptions = {
     origin: allowedOrigin,
-    origin: true,
     credentials: true
-};
-app.use(cors(corsOptions));
+
+}; */
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(logMiddleware);
 app.use("/songs", songs);
